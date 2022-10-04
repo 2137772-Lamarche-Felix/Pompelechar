2fe70bb1cc9d5f90d00d858df8e27a3b2ee16ef0
Oli Bellerose <89463306+2111325-Bellerose-Olivier@users.noreply.github.com>
2022-10-04 15:17:02 -0400

    Add files via upload

--- VoitureMousse/Assets/Scripts/Pompe.gd
+++ VoitureMousse/Assets/Scripts/Pompe.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+onready var police = get_node("policeSprite")
+
+
 func _on_Pompe_body_entered(body: Area2D) -> void:
-	queue_free()
+	$explosion2.shot()
+	police.visible = false
 	Global.update_pompe(1)
+
+
---
+++ VoitureMousse/Assets/Scripts/explosion.gd
@@ -0,0 +1,13 @@
+extends Particles2D
+
+	
+
+
+
+func shot():
+	emitting = true
+	one_shot = true
+
+
+
+
