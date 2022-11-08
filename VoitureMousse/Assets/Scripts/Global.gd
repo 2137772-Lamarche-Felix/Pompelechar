faa3cfaf738d0a82c720decfd9b1cda63562c4cc
Oli <2111325@etudiant.cegepvicto.ca>
2022-11-08 14:22:10 -0500

    premier commit de l'olive, j'ai probabement tout brisé

--- VoitureMousse/Assets/Scripts/Global.gd
+++ VoitureMousse/Assets/Scripts/Global.gd
@@ -7,6 +7,12 @@ var Pompe =0
 
 signal Pompe_changed(Pompe)
 
+func _process(delta):
+	if Input.is_action_pressed("ui_2"):
+		get_tree().change_scene("res://Assets/Scenes/Niveau2.tscn")
+	if Input.is_action_pressed("ui_1"):
+		get_tree().change_scene("res://Assets/Scenes/MainZZtope.tscn")
+
 func update_pompe(delta):
 	Pompe+=delta
 	if Pompe >= 45:
